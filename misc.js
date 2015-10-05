50e847cfa0b9ad591129c6e5a27222e4f416c432
EKracht <ethan1kracht@gmail.com>
2015-10-04 21:41:20 -0700

    Update misc.js

--- misc.js
+++ misc.js
@@ -1,12 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    var element = document.getElementById('container');
-    var height = element.offsetHeight;
-    if (height < screen.height) {
-        document.getElementById("footer").classList.add('stikybottom');
-    }
-}, false);
-
- (function() {
+document.ready(function() {
     var offsetHeight = document.getElementById('container').offsetHeight;   
     var screenHeight = screen.height;
 
